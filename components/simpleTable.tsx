785bd7543c062ed23d810c05d3d49d0c2a2d2f7b
Fernando Tadeo <fernandotadeos@gmail.com>
2025-02-11 18:06:25 -0300

    FIX tabla ventas

--- components/simpleTable.tsx
+++ components/simpleTable.tsx
@@ -95,32 +95,6 @@ export default function SimpleTable() {
 
   return (
     <div className="flex flex-col gap-3">
-      {/* Buscador */}
-      <div className="mb-4">
-        <Input
-          isClearable
-          className="w-full sm:max-w-[44%]"
-          placeholder="Buscar por ID, revendedor, fecha, monto, zona, dirección..."
-          startContent={
-            <svg
-              className="w-5 h-5 text-gray-500"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          }
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
-
       <Table
         selectionMode="single"
         aria-label="Tabla de últimas ventas"
